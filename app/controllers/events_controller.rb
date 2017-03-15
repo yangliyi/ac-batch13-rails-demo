d3ed5528f10cce5ad885347f3847c811896f4c56
Yang Li-Yi <yangliyi1228@gmail.com>
2017-03-15 19:54:38 +0800

    One to many: can select category for event

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -69,6 +69,6 @@ class EventsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def event_params
-      params.require(:event).permit(:name, :descrption, :is_public, :capacity)
+      params.require(:event).permit(:name, :descrption, :is_public, :capacity, :category_id)
     end
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,6 @@
 class Event < ApplicationRecord
 	has_many :attendees
+	belongs_to :category, optional: true
+
+	delegate :name, :to => :category, :prefix => true, :allow_nil => true
 end
